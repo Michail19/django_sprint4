d74777e1489fe90f1fada15ce10e061bb19482e6
Michail19 <Mi.ershov.2017@yandex.ru>
2026-02-12 23:21:22 +0300

    Squashed commit of the following:
    commit 566fea965960df9eb2ba6e42d1f63d92fba1fb3f
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Thu Feb 12 23:19:57 2026 +0300
    
        Fix full
    
    commit e444a12567acc6ab2c295d7f75642925d36150e0
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Thu Feb 12 23:16:27 2026 +0300
    
        u

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,172 +1,99 @@
-from django.contrib.auth.models import User
-from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils import timezone
+from django.db import models
+
+User = get_user_model()
+TEXT_LENGTH = 256
 
 
-class PublishedModel(models.Model):
-    """Абстрактная модель для полей is_published и created_at."""
-
+class BaseModel(models.Model):
     is_published = models.BooleanField(
-        'Опубликовано',
         default=True,
+        verbose_name='Опубликовано',
         help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
     created_at = models.DateTimeField(
-        'Добавлено',
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name='Добавлено'
     )
 
     class Meta:
         abstract = True
 
 
-class Category(PublishedModel):
-    """Модель тематической категории."""
-
-    title = models.CharField(
-        'Заголовок',
-        max_length=256
-    )
-    description = models.TextField('Описание')
+class Category(BaseModel):
+    title = models.CharField(max_length=TEXT_LENGTH, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(
-        'Идентификатор',
         unique=True,
-        help_text=(
-            'Идентификатор страницы для URL; '
-            'разрешены символы латиницы, цифры, дефис и подчёркивание.'
-        )
+        verbose_name='Идентификатор',
+        help_text='Идентификатор страницы для URL; разрешены символы '
+                  'латиницы, цифры, дефис и подчёркивание.'
     )
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
 
-    def __str__(self):
-        return self.title
 
-
-class Location(PublishedModel):
-    """Модель географической метки."""
-
-    name = models.CharField(
-        'Название места',
-        max_length=256
-    )
+class Location(BaseModel):
+    name = models.CharField(max_length=TEXT_LENGTH,
+                            verbose_name='Название места')
 
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
 
-    def __str__(self):
-        return self.name
 
-
-class Post(PublishedModel):
-    """Модель публикации."""
-
-    title = models.CharField(
-        'Заголовок',
-        max_length=256
-    )
-    text = models.TextField('Текст')
+class Post(BaseModel):
+    title = models.CharField(max_length=TEXT_LENGTH, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
-        'Дата и время публикации',
-        help_text=(
-            'Если установить дату и время в будущем — '
-            'можно делать отложенные публикации.'
-        )
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем — '
+                  'можно делать отложенные публикации.'
     )
+    image = models.ImageField('Фото', upload_to='posts_images', blank=True)
     author = models.ForeignKey(
-        get_user_model(),
+        User,
         on_delete=models.CASCADE,
         verbose_name='Автор публикации',
-        related_name='blog_posts'
+        related_name='posts'
     )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
         null=True,
-        blank=True,
-        verbose_name='Местоположение'
+        verbose_name='Местоположение',
+        related_name='posts'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name='Категория'
-    )
-    # Добавляем поле для изображений
-    image = models.ImageField(
-        'Изображение',
-        upload_to='posts_images/',
-        blank=True,
-        null=True,
-        help_text='Загрузите изображение для публикации'
+        verbose_name='Категория',
+        related_name='posts'
     )
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
-        ordering = ['-pub_date']
-        # Для обеспечения уникальности slug (если будете использовать)
-        # unique_together = ['author', 'title']
-
-    def __str__(self):
-        return self.title
-
-    @property
-    def comment_count(self):
-        """Количество комментариев к посту."""
-        return self.comments.count()
-
-    def is_published_now(self):
-        """Проверка, опубликован ли пост в данный момент."""
-        return self.is_published and self.pub_date <= timezone.now()
-
-    def can_be_viewed_by(self, user):
-        """
-        Проверка, может ли пользователь просматривать пост.
-        Автор видит все свои посты, остальные - только опубликованные.
-        """
-        if user == self.author:
-            return True
-        return self.is_published_now() and self.category.is_published
 
 
 class Comment(models.Model):
-    """Модель комментария к публикации."""
-
-    text = models.TextField(
-        'Текст комментария',
-        help_text='Введите текст комментария'
-    )
-
+    text = models.TextField('Текст комментария')
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
-        related_name='comments'
+        verbose_name='Комментарий',
+        related_name='comments',
     )
-
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='blog_comments'
-    )
-
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
         ordering = ('created_at',)
 
-
-    def __str__(self):
-        return f'Комментарий {self.author} к посту "{self.post.title}"'
-
-    def can_be_edited_by(self, user):
-        """Проверка, может ли пользователь редактировать комментарий."""
-        return user == self.author
-
-    def can_be_deleted_by(self, user):
-        """Проверка, может ли пользователь удалить комментарий."""
-        return user == self.author
+    def __str__(self) -> str:
+        return self.text
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,36 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
-class AboutView(TemplateView):
-    """Страница 'О нас' - CBV версия."""
-    template_name = 'pages/about.html'
-
-
-class RulesView(TemplateView):
-    """Страница 'Правила' - CBV версия."""
-    template_name = 'pages/rules.html'
-
-
-class CsrfFailureView(TemplateView):
-    template_name = 'pages/403csrf.html'
-    status_code = 403
-
-    def render_to_response(self, context, **response_kwargs):
-        response_kwargs.setdefault('status', self.status_code)
-        return super().render_to_response(context, **response_kwargs)
-
-
-def csrf_failure(request, reason=''):
-    """Кастомная страница ошибки CSRF."""
-    return render(request, 'pages/403csrf.html', status=403)
 
 
 def page_not_found(request, exception):
-    """Кастомная страница 404."""
     return render(request, 'pages/404.html', status=404)
 
 
-def server_error(request):
-    """Кастомная страница 500."""
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def internal_server_error(request):
     return render(request, 'pages/500.html', status=500)
